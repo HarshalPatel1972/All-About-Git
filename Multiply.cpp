41b4b73ba45dcdd9e86f2dfcca16dfac1a384bf5
Harshal Patel <hp842484n@gmail.com>
2023-02-25 12:39:12 +0530

    added implementation for Multiply.cpp

---
+++ Multiply.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2, prod;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    prod = num1 * num2;
+
+    cout << "Product of " << num1 << " and " << num2 << " is " << prod << endl;
+
+    cout << endl;
+
+    //Adding comment
+
+    return 0;
+}
+
